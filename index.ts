8db6b140b48bbadc301f289c081116749cfc73e9
Huzaifa Qazi <160002762+huzaifaqazi@users.noreply.github.com>
2024-03-30 23:11:42 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bni/env node
+import inquirer from "inquirer"
+
+const answer = await inquirer.prompt([
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operator to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "subtraction", "multiplication", "division"],
+  },
+]);
+if(answer.operator==="addition"){
+  console.log(answer.firstNumber + answer.secondNumber)
+}else if(answer.operator==="subtraction"){
+  console.log(answer.firstNumber - answer.secondNumber)
+}else if(answer.operator==="multiplication"){
+  console.log(answer.firstNumber * answer.secondNumber)
+}else if(answer.operator==="division"){
+  console.log(answer.firstNumber / answer.secondNumber)
+}else{
+  console.log("plz check operator")
+}
